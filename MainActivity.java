f611d0e4415044c63fece7cd16698e67b8612c46
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-31 01:00:55 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        // Web sayfasını yükleyin
+        webView.loadUrl("http://alpbeyazgul.com");
+    }
+}
